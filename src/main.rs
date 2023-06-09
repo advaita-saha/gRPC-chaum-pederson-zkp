a49ee6080fe4fb405359d56984e5e5cbee50b19a
Advaita Saha <adv11sphs@gmail.com>
2023-06-09 21:11:30 +0530

    feat: exponentiation added along with tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+fn exponentiate(num: u32, exp: u32, p:u32) -> u32 {
+    num.pow(exp) % p
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+// tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_exponentiate(){
+        assert_eq!(exponentiate(1, 2, 11), 1);
+        assert_eq!(exponentiate(2, 3, 11), 8);
+        assert_eq!(exponentiate(2, 4, 11), 5);
+    }
 }
