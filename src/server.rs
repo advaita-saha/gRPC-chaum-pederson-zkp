1d5765caa3f3cd6b3544a22d705d66833b2ee18b
Advaita Saha <adv11sphs@gmail.com>
2023-06-12 01:54:58 +0530

    feat: verification challenge function in server completed

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,7 @@
+use chaum_pederson_zkp::{random_number, random_string, verify, G, H, P};
 use std::collections::HashMap;
 use std::sync::Mutex;
-use tonic::{transport::Server, Request, Response, Status};
+use tonic::{transport::Server, Code, Request, Response, Status};
 
 pub mod zkp_auth {
     include!("./zkp_auth.rs");
@@ -24,8 +25,8 @@ pub struct UserInfo {
 
 #[derive(Debug, Default)]
 pub struct AuthImpl {
-    pub user_info: Mutex<HashMap<String, UserInfo>>,
-    pub auth_info: Mutex<HashMap<String, String>>,
+    pub user_info: Mutex<HashMap<String, UserInfo>>, // user_id -> user_info
+    pub auth_info: Mutex<HashMap<String, String>>,   // auth_id -> user_id
 }
 
 #[tonic::async_trait]
@@ -66,41 +67,75 @@ impl Auth for AuthImpl {
         let user_id = request.user;
 
         let user_info_hashmap = &mut self.user_info.lock().unwrap(); // TODO : improve
-        if let Some(user_info) = user_info_hashmap.get(&user_id) {
+        if let Some(user_info) = user_info_hashmap.get_mut(&user_id) {
+            let auth_id = random_string(6); // TODO : generate auth_id
+            let c = random_number() % 10; // TODO : generate c
 
-            let auth_id = String::new();    // TODO : generate auth_id
-            let c = 0u32;                      // TODO : generate c
+            user_info.r1 = request.r1;
+            user_info.r2 = request.r2;
+            user_info.c = c;
 
-            let user_info = UserInfo {
-                y1: user_info.y1,
-                y2: user_info.y2,
-                r1: request.r1,
-                r2: request.r2,
-                c,
-                session_id: String::new(),
-            };
-
-            user_info_hashmap.insert(user_id, user_info);
+            let auth_info_hashmap = &mut self.auth_info.lock().unwrap(); // TODO : improve
+            auth_info_hashmap.insert(auth_id.clone(), user_id.clone());
 
             Ok(Response::new(AuthenticationChallengeResponse {
                 auth_id,
                 c,
             }))
         } else {
-            todo!()
+            Err(Status::new(
+                Code::NotFound,
+                format!("User {} not found", user_id),
+            ))
         }
-
-        
-
-       
     }
 
     async fn verify_authentication(
         &self,
         request: Request<AuthenticationAnswerRequest>,
     ) -> Result<Response<AuthenticationAnswerResponse>, Status> {
-        println!("Got a request: {:?}", request);
-        todo!()
+        println!("Got a Verification Request : ");
+
+        let request = request.into_inner();
+        let auth_id = request.auth_id;
+        let auth_info_hashmap = &mut self.auth_info.lock().unwrap(); // TODO : improve
+        let user_info_hashmap = &mut self.user_info.lock().unwrap(); // TODO : improve
+
+        if let Some(user_id) = auth_info_hashmap.get(&auth_id) {
+            if let Some(user_info) = user_info_hashmap.get_mut(user_id) {
+                // Verification Code
+                let r1 = user_info.r1;
+                let r2 = user_info.r2;
+                let c = user_info.c;
+                let y1 = user_info.y1;
+                let y2 = user_info.y2;
+                let s = request.s;
+
+                if verify(P, y1, y2, r1, r2, G, H, c, s) {
+                    let session_id = random_string(6);
+                    user_info.session_id = session_id.clone();
+
+                    println!("{:?}", user_info_hashmap);
+
+                    Ok(Response::new(AuthenticationAnswerResponse { session_id }))
+                } else {
+                    Err(Status::new(
+                        Code::Unauthenticated,
+                        format!("Challenge not solved correctly"),
+                    ))
+                }
+            } else {
+                return Err(Status::new(
+                    Code::NotFound,
+                    format!("User {} not found", user_id),
+                ));
+            }
+        } else {
+            Err(Status::new(
+                Code::NotFound,
+                format!("Auth {} not found", auth_id),
+            ))
+        }
     }
 }
 
